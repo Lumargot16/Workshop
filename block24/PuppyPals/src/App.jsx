21fa17450f1de31d70023e9c1aa445b1dba06978
Lumargot16 <amarista61@gmail.com>
2023-09-23 21:46:13 -0400

    Updated file

--- block24/PuppyPals/src/App.jsx
+++ block24/PuppyPals/src/App.jsx
@@ -1,35 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { puppyList } from './data'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
+  const styles = {
+    cardlist: {backgroundColor: 'white', color:'black', padding:'10px 20px'},
+    paragraph: {cursor:'pointer'},
+    list: {listStyle:'none', display:'flex', flexDirection: 'column', justifyContent:'center', alignItems: 'center', margin:'0', padding: '0'}
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+      <div className="App">
+        {
+          puppies.map((puppy) => {
+            return (
+            <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id} style={styles.paragraph}>{puppy.name}</p>
+            );
+          })
+        }
+        {featPupId && (
+          <div style={styles.cardlist}>
+            <h2>{featuredPup.name}</h2>
+            <ul style={styles.list}>
+              <li>Age:{featuredPup.age}</li>
+              <li>Email:{featuredPup.email}</li>
+              </ul>
+              </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      
+    );
+
 }
 
 export default App
+
+
